29bc06dc64e757d9616ce90419673db3aed705cc
Ryan Pearson <ryanpinpb@gmail.com>
2020-10-30 23:51:08 -0700

    removed special styles from thank you

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,12 +1,10 @@
 import React from "react"
 
-import { useColorMode, Heading, Flex } from "@chakra-ui/core"
+import { Heading, Flex } from "@chakra-ui/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Thanks = () => {
-  const { colorMode } = useColorMode()
-
   return (
     <Layout>
       <SEO
@@ -16,23 +14,26 @@ const Thanks = () => {
       />
       <Heading
         as="h1"
-        id="thanks"
-        position={{ base: "relative", md: "absolute" }}
+        pb="4"
+        // position={{ base: "relative", md: "absolute" }}
+        // position={{ base: "relative", md: "absolute" }}
         textAlign="center"
-        color={{
-          base: "",
-          md:
-            colorMode === "light" ? "rgba(0, 0, 0, 0)" : "rgba(255,255,255,1)",
-        }}
-        opacity={{ base: 1, md: ".03" }}
+        // color={{
+        //   base: "",
+        //   md:
+        //     colorMode === "light" ? "rgba(0, 0, 0, 0)" : "rgba(255,255,255,1)",
+        // }}
+        // opacity={{ base: 1, md: ".03" }}
         lineHeight="1.1"
-        top={{ base: "0", md: "50%" }}
-        left={{ base: "0", md: "50%" }}
-        transform={{ base: "translate3D(0)", md: "translate3D(-50%,-50%,0)" }}
+        // top={{ base: "0", md: "50%" }}
+        // left={{ base: "0", md: "50%" }}
+        // transform={{ base: "translate3D(0)", md: "translate3D(-50%,-50%,0)" }}
       >
         THANK YOU
       </Heading>
-      <Flex>Form submission received. Please continue to browse the site</Flex>
+      <Flex justify="center" textAlign="center">
+        Form submission received, we will reach out to you soon.
+      </Flex>
     </Layout>
   )
 }
